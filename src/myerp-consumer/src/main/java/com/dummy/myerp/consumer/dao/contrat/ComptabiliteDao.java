9c1a0607a89fd2b4a2bec2c6d6c59c1cb4fc1655
Nyhn <37303530+Nyhn@users.noreply.github.com>
2020-08-11 10:36:01 +0200

    Update ComptabiliteDao.java
    Ajout d'une méthode interface : getListSequenceEcritureComptable

--- src/myerp-consumer/src/main/java/com/dummy/myerp/consumer/dao/contrat/ComptabiliteDao.java
+++ src/myerp-consumer/src/main/java/com/dummy/myerp/consumer/dao/contrat/ComptabiliteDao.java
@@ -5,6 +5,7 @@ import java.util.List;
 import com.dummy.myerp.model.bean.comptabilite.CompteComptable;
 import com.dummy.myerp.model.bean.comptabilite.EcritureComptable;
 import com.dummy.myerp.model.bean.comptabilite.JournalComptable;
+import com.dummy.myerp.model.bean.comptabilite.SequenceEcritureComptable;
 import com.dummy.myerp.technical.exception.NotFoundException;
 
 
@@ -80,4 +81,15 @@ public interface ComptabiliteDao {
      * @param pId l'id de l'écriture
      */
     void deleteEcritureComptable(Integer pId);
+
+    // ==================== SequenceEcritureComptable ====================
+
+    /**
+     * Renvoie la Séquence d'Écriture Comptable d'id {@code pJournalCode} et {@code pAnnee}
+     * @param pJournalCode le code de la séquence d'écriture comptable
+     * @param pAnnee l'année de la séquence d'écriture comptable
+     * @return {@link SequenceEcritureComptable}
+     * @throws NotFoundException si aucune séquence d'écriture comptable n'est trouvée
+     */
+    SequenceEcritureComptable getListSequenceEcritureComptable(String pJournalCode,Integer pAnnee) throws NotFoundException;
 }
